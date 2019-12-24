fc19f6223bb1df52de868a6aeaa580a08097f7e6
pankajkachhwaye <pankajkachhwaye@gmail.com>
2019-12-24 15:59:56 +0530

    User and article restrictions on the basis of action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def new()
     @article = Article.new
@@ -11,7 +13,7 @@ class ArticlesController < ApplicationController
   def create()
     #render plain: params[:article].inspect
     @article = Article.create(article_param)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = 'Article created sucessfully'
       redirect_to article_path(@article)
@@ -50,6 +52,13 @@ class ArticlesController < ApplicationController
 
     def article_param
       params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger] = "You can only edit or delete your own articles"
+        redirect_to root_path
+      end
     end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
+  before_action :require_same_user, only: [:edit, :update]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 3)
@@ -12,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "welcome to the Aplha-blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -42,5 +44,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:username, :email, :password)    
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:danger] = "You can only edit your own account"
+        redirect_to root_path
+      end
     end
 end
